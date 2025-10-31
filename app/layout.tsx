ab734d9d492cc8db127a7ffc25ff10a6b5cdfd38
bentechrw <rollwittechnology@gmail.com>
2025-10-31 22:20:04 +0200

    Refactor styles and layout: update font settings and clean up Home component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Teachers } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const teachersMono = Teachers({
+  variable: "--font-teachers-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${teachersMono.variable} antialiased`}
       >
         {children}
       </body>
